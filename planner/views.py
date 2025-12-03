71ac93333560238bdf4d047c6e3d63445c7f73a9
Kenny Bacdayan <kbacdayan@my.waketech.edu>
2025-12-03 08:02:47 -0500

    updates

--- planner/views.py
+++ planner/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView
+from .models import Recipe, MealPlanDay, ShoppingList
+
+
+class RecipeListView(LoginRequiredMixin, ListView):
+    model = Recipe
+    template_name = "planner/recipe_list.html"
+    context_object_name = "recipes"
+
+    def get_queryset(self):
+        # only show recipes for the logged-in user
+        return Recipe.objects.filter(user=self.request.user).order_by("name")
+
+
+class RecipeDetailView(LoginRequiredMixin, DetailView):
+    model = Recipe
+    template_name = "planner/recipe_detail.html"
+    context_object_name = "recipe"
+
+    def get_queryset(self):
+        return Recipe.objects.filter(user=self.request.user)
+
+
+class MealPlanDayDetailView(LoginRequiredMixin, DetailView):
+    model = MealPlanDay
+    template_name = "planner/mealplan_detail.html"
+    context_object_name = "day"
+
+    def get_queryset(self):
+        return MealPlanDay.objects.filter(user=self.request.user)
+
+
+class ShoppingListDetailView(LoginRequiredMixin, DetailView):
+    model = ShoppingList
+    template_name = "planner/shoppinglist_detail.html"
+    context_object_name = "shopping_list"
+
+    def get_queryset(self):
+        return ShoppingList.objects.filter(user=self.request.user)
+
 
 # Create your views here.
